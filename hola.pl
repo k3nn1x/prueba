0662e35b1f16be8938538e5dc236782225058520
Eliezer J. Romero C <eliezer@(none)>
2014-05-24 23:47:39 +0000

    pruebas con perl utilizando padre

---
+++ hola.pl
@@ -0,0 +1,11 @@
+use 5.010;
+use strict;
+use warnings;
+say "Introduce un numero";
+my $num1 = <STDIN>;
+chomp $num1;
+say "Introduce otro";
+my $num2= <STDIN>;
+chomp $num2;
+my $total = $num1+$num2;
+say "El Total es: $total";
